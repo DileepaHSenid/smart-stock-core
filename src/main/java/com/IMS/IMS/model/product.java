f588621ac6e16a547bdbd07b90beb7e4d6aab3bc
HSenid-AjDep <anjana.d@hsenidlanka.com>
2024-07-29 09:52:47 +0530

    Initial backend setup

---
+++ src/main/java/com/IMS/IMS/model/product.java
@@ -0,0 +1,13 @@
+package com.IMS.IMS.model;
+
+public class product {
+    private String id;
+    private String name;
+    private String Status;
+    private String ShippingId;
+    private String description;
+    private double price;
+    private int stockQuantity;
+    private String category;
+    private String supplierId;
+}
