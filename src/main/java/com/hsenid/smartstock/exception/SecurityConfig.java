1dc37df982fa39d45612d61b5ec502f0a04253ef
AlthafNaz <althaf.n@hsenidlanka.com>
2024-08-06 12:04:42 +0530

    Added DeleteMapping to Supplier

--- src/main/java/com/hsenid/smartstock/exception/SecurityConfig.java
+++ src/main/java/com/hsenid/smartstock/exception/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         // Configure HTTP security
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/home", "/signin", "/signup","/orders","/stocks","/stocks/{id}","/users","/suppliers","/suppliers/createsupplier/").permitAll()    // all can access
+                        .requestMatchers("/home", "/signin", "/signup","/orders","/stocks","/stocks/{id}","/users","/suppliers","/suppliers/createsupplier/","/suppliers/{id}").permitAll()    // all can access
                         // Require authentication for any other request
 
 //                        .requestMatchers("/orders").hasAuthority("ADMIN")
@@ -46,6 +46,7 @@ public class SecurityConfig {
 //                        .requestMatchers("/CreateSupplier").hasAuthority(UserRole.ADMIN.name())
                         .requestMatchers("/products/create").hasAuthority(UserRole.ADMIN.name())
                         .requestMatchers("/CreateProduct").hasAuthority(UserRole.ADMIN.name())
+//                        .requestMatchers("/suppliers/{id}").hasAuthority(UserRole.ADMIN.name())
 //                        .requestMatchers(HttpMethod.POST, "/supplier").hasRole("ADMIN")
 
                         .anyRequest().authenticated())
