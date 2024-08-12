2b3f2b83f392fcfdc5d64f297b9d1e20167e0310
AlthafNaz <althaf.n@hsenidlanka.com>
2024-08-12 12:50:45 +0530

    Update

--- src/main/java/com/hsenid/smartstock/dto/request/SupplierRequest.java
+++ src/main/java/com/hsenid/smartstock/dto/request/SupplierRequest.java
@@ -1,15 +1,10 @@
 package com.hsenid.smartstock.dto.request;
 
-import lombok.Data;
-
-@Data
-public class SupplierRequest {
-    private String img;
-    private String firstName;
-    private String lastName;
-    private String contactPerson;
-    private String email;
-    private String phone;
-    private String address;
-
-}
+public record SupplierRequest (
+        String FirstName,
+        String LastName,
+        String contactPerson,
+        String email,
+        String phone,
+        String address
+){}
